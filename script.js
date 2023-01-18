960e51e9f762e6ea689c0f8473cd433f9149bcd1
MargaritaBraun <119820661+MargaritaBraun@users.noreply.github.com>
2023-01-18 17:29:33 +0300

    feat: done hamburger menu

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 console.log('1 Вёрстка валидная +10 = 5 баллов/n2 Вёрстка семантическая +20 = 20 баллов/n3 Вёрстка соответствует макету +48 = 20 баллов/n4 Требования к css + 12 = 6 баллов/n5 Интерактивность, реализуемая через css +20 = 10 баллов/nИтого = 61 балла')
+let hamburger = document.querySelector('.hamburger');
+let nav__menu = document.querySelector('.nav__menu');
+hamburger.addEventListener('click', function(){
+	nav__menu.classList.toggle('active');
+})
