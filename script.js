03d7eb426983f588bcb9f08cb8d82d70e22abc22
MargaritaBraun <119820661+MargaritaBraun@users.noreply.github.com>
2023-02-07 21:33:22 +0300

    fix: humburger and Prices

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 console.log('Вёрстка соответствует макету. Ширина экрана 768px +24 /n Вёрстка соответствует макету. Ширина экрана 380px +24 /n Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +15 /n На ширине экрана 380рх и меньше реализовано адаптивное меню +22 (Допускается появление адаптивного меня на ширине более 380, но не допускается на ширине более 770px)')
-let hamburger = document.querySelector('.hamburger');
-let nav__menu = document.querySelector('.nav__menu');
-hamburger.addEventListener('click', function(){
-	nav__menu.classList.toggle('active');
-})
+const hamburger = document.querySelector('.hamburger');
+const nav__menu = document.querySelector('.nav__menu');
+
+//Prices
+
+const accordionTitles = document.querySelectorAll(".pricesButton");
+
+accordionTitles.forEach((accordionTitle) => {
+  accordionTitle.addEventListener("click", () => {
+    if (accordionTitle.classList.contains("active")) {
+      accordionTitle.classList.remove("active");
+    } else {
+      const accordionTitlesWithIsOpen = document.querySelectorAll(".active");
+      accordionTitlesWithIsOpen.forEach((accordionTitleWithIsOpen) => {
+        accordionTitleWithIsOpen.classList.remove("active");
+      });
+      accordionTitle.classList.add("active");
+    }
+  });
+});
