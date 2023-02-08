19e675de1381adf6dc71e3341d5a1f22f598bd45
MargaritaBraun <119820661+MargaritaBraun@users.noreply.github.com>
2023-02-08 05:43:15 +0300

    fix:service done

--- script.js
+++ script.js
@@ -19,3 +19,120 @@ accordionTitles.forEach((accordionTitle) => {
     }
   });
 });
+
+//Servise   service
+let gardenbtn = document.getElementById('garden');
+let lawbtn = document.getElementById('law');
+let plantingbtn = document.getElementById('planting');
+let gardenGallary = document.querySelectorAll('.garden');
+let lawGallary = document.querySelector('.lawn');
+let plantingGallary = document.querySelectorAll('.planting');
+
+
+const addTagsClickHandler = () => {
+    document.querySelector('.service__container').addEventListener('click', (e) =>{
+        if (e.target.classList.contains('servicebtn')) {
+            let clickedTag = e.target;
+            clickedTag.classList.toggle('active');
+            let gallary = document.querySelectorAll('.element')
+            gallary.forEach(gallar => {
+                gallar.classList.add('blur');
+            })
+            selectClickButtonsGarden();
+            selectClickButtonsLaw();
+            selectClickButtonsPlants();
+        }
+    })
+}
+
+addTagsClickHandler();
+
+const selectClickButtonsGarden = () => {
+        if (gardenbtn.classList.contains('active')) {
+            gardenGallary.forEach(garden => {
+                garden.classList.remove('blur');
+            })
+            if (!gardenbtn.classList.contains('active')) {
+                garden.classList.add('blur');
+            }
+        }
+    }
+
+    const selectClickButtonsLaw = () => {
+        if (lawbtn.classList.contains('active')) {
+            lawGallary.classList.remove('blur');
+            }
+            if (!lawbtn.classList.contains('active')) {
+                lawGallary.classList.add('blur');
+            }
+        }
+
+
+        const selectClickButtonsPlants = () => {
+            if (plantingbtn.classList.contains('active')) {
+                plantingGallary.forEach(plantin => {
+                    plantin.classList.remove('blur');
+                })
+                if (!plantingbtn.classList.contains('active')) {
+                    plantin.classList.add('blur');
+                }
+            }
+        }
+
+    /*
+
+
+
+
+
+const addblurAll = () => {
+    let tags = document.querySelectorAll('.element');
+    tags.classList.add('.blur')
+}
+//const gardenButtonsAll = () => {
+    
+    gardenbtn.addEventListener ('click', () => {
+        gallaryGarden.classList.remove('.blur')
+    })
+    //console.log(gallaryGarden);
+//}
+
+
+
+
+//удалили выбранный стиль для кнопки а добавили обычный класс, можно мне удалить
+const removeSelectedTags = () => {
+    let tags = document.querySelectorAll('.servicebtn');
+    tags.forEach(tag => {
+        tag.classList.remove('servicebtnActive');
+        tag.classList.add('btn__not');
+        if (clickedTag.innerText) {
+            showAllStrategies();
+        } else {
+            filtlerStrategies(clickedTag.innerText);
+        }
+        
+    })
+
+}
+/*const selectClickTag = (clickedTag) => {
+clickedTag.classList.add('servicebtnActive');
+clickedTag.classList.remove('btn__not');
+}
+
+const showAllStrategies = () => {
+
+}
+
+const filtlerStrategies = (selectedTag) => {
+    let strategies = document.querySelectorAll('.tags');
+    strategies.forEach(stratedy => {
+        stratedy.classList.add('blur');
+        stratedy.querySelectorAll('.tags').forEach(tag => {
+            if (tag.innerText === selectedTag) {
+                stratedy.classList.remove('blur');
+            }
+        })
+    })
+}
+*/
